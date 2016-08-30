75d91453c53a50e61c97b24ecbc29b5c4bf12d67
rbailey01 <randy.l.bailey@comcast.net>
2016-08-30 15:05:12 -0600

    fixed variable name

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -100,7 +100,7 @@ func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte
         return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the variable and value to set")
     }
 
-    randyName = args[0]                            //rename for fun
+    name = args[0]                            //rename for fun
     value = args[1]
     err = stub.PutState(name, []byte(value))  //write the variable into the chaincode state
     if err != nil {
